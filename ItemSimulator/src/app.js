c2fb54b3e60fe0140d26d0a766ab4fd8ae822d4d
성원 정 <tyhg56@naver.com>
2024-09-09 17:36:01 +0900

    Fix : " app.js 수정 ( 서버 오픈 기초 작업 )
    app.js
     - express 객체 가져와서 app에 저장
    
     - 미들웨어 등록 ( json, cookieParser, router 등록 )
    
     - PORT 번호 3020으로 해서 서버 오픈

---
+++ ItemSimulator/src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import UsersRouter from './routes/users.router.js';
+
+const app = express();
+const PORT = 3020;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/ItemSimulator', [UsersRouter]);
+
+app.listen(PORT, () => {
+    console.log(PORT, "아이템 시뮬레이터 서버 열림");
+});
