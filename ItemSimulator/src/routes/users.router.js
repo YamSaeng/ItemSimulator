07c71310413da947e6239ec6afc3419e95cae643
성원 정 <tyhg56@naver.com>
2024-09-09 18:26:44 +0900

    Fix : " 회원가입 기능 추가 "
    users.router.js 수정
     - id, password, confirmPassword 를 입력받아 회원가입 진행
    
     - 정규식으로 id 검사 ( 소문자 영어 + 숫자 )
    
     - bcrypt를 이용해 password를 암호화 처리
    
     - user 생성해서 DB에 저장
    
     - 클라이언트에게 회원가입한 id 정보 반환

--- ItemSimulator/src/routes/users.router.js
+++ ItemSimulator/src/routes/users.router.js
@@ -1,5 +1,48 @@
 import express from 'express';
+import { prisma } from '../utils/prisma/prismaClient.js'
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
+
+router.post('/sign-up', async (req, res, next) => {
+    const { id, password, confirmPassword } = req.body;
+    const isExistUser = await prisma.users.findFirst({
+        where: {
+            id,
+        },
+    });
+
+    const engNumIdRule = /^(?=[a-za-z])(?=.*[0-9]).{2,10}$/;
+    if (!engNumIdRule.test(id)) {
+        return res.status(409).json({ message: '소문자 영어와 숫자를 조합해 입력하세요 ( 최소 2글자, 최대 10글자 )' });
+    }
+
+    if (confirmPassword === undefined) {
+        return res.status(409).json({ message: '비밀번호 확인을 입력하세요' });
+    }
+
+    if (password.length < 6) {
+        return res.status(409).json({ message: '비밀번호는 최소 6자 이상이 되어야 합니다.' });
+    }
+
+    if (password != confirmPassword) {
+        return res.status(409).json({ message: '비밀번호와 비밀번호 확인이 일치하지 않습니다.' });
+    }
+
+    if (isExistUser) {
+        return res.status(409).json({ message: '이미 존재하는 아이디입니다.' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.users.create({
+        data: {
+            id,
+            password: hashedPassword,
+        },
+    });
+
+    return res.status(201).json({ message: `${id}로 회원가입이 완료되었습니다.` });
+});
 
 export default router;
