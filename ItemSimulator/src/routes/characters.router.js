991f35c41563fdfd0d0856659d18c6d9e657ee16
성원 정 <tyhg56@naver.com>
2024-09-10 18:05:58 +0900

    Fix : " 캐릭터 검색 추가 "
    chracters.router.js
     - router.delete() 받아오는 characterId의 이름을 deleteCharacterId 변경
    
     - routet.get('/character-search/:searchCharacterId) 추가 ( 캐릭터 검색 )
      - 찾고자 하는 캐릭터가 characters DB에 없으면 에러 반환
    
      - 찾은 캐릭터가 내 캐릭터가 아니면 money 컬럼을 반환하지 않음
      - 찾은 캐릭터가 내 캐릭터면 money도 같이 반환

--- ItemSimulator/src/routes/characters.router.js
+++ ItemSimulator/src/routes/characters.router.js
@@ -14,7 +14,7 @@ const accessTokenSecretKey = process.env.ACCESS_TOKEN_SECRET_KEY;
 const refreshTokenSecretKey = process.env.REFRESH_TOKEN_SECRET_KEY;
 
 router.post('/character-create', authMiddleware, async (req, res, next) => {
-    const { characterName } = req.body;       
+    const { characterName } = req.body;
 
     const isExistCharacter = await prisma.characters.findFirst({
         where: { characterName: characterName }
@@ -41,12 +41,12 @@ router.post('/character-create', authMiddleware, async (req, res, next) => {
         .json({ message: `${characterName} 캐릭터를 만들었습니다.` });
 });
 
-router.delete('/character-delete/:characterId', authMiddleware, async (req, res, next) => {
-    const { characterId } = req.params;
+router.delete('/character-delete/:deleteCharacterId', authMiddleware, async (req, res, next) => {
+    const { deleteCharacterId } = req.params;
 
     const deleteCharacter = await prisma.characters.findFirst({
         where: {
-            characterId: +characterId
+            characterId: +deleteCharacterId
         }
     });
 
@@ -61,20 +61,71 @@ router.delete('/character-delete/:characterId', authMiddleware, async (req, res,
         return res
             .status(401)
             .json({ message: `${deleteCharacter.characterName}은 내 계정의 캐릭터가 아닙니다.` });
-    }    
+    }
 
     const deleteCharacterName = deleteCharacter.characterName;
 
     await prisma.characters.delete({
         where: {
-            characterId: +characterId
+            characterId: +deleteCharacterId
         }
-    });    
+    });
 
-    
     return res
         .status(200)
         .json({ message: `${deleteCharacterName} 캐릭터가 삭제되었습니다.` });
+});
+
+router.get('/character-search/:searchCharacterId', authMiddleware, async (req, res, next) => {
+    const { searchCharacterId } = req.params;
+
+    // 검색할 캐릭터를 characters DB에서 찾음
+    const searchCharacter = await prisma.characters.findFirst({
+        where: {
+            characterId: +searchCharacterId
+        },
+        select: {
+            userId: true,
+            characterName: true,
+            hp: true,
+            str: true,
+            dex: true,
+            int: true,
+            money: true
+        }
+    });
+
+    // 못찾으면 에러 반환
+    if (!searchCharacter) {
+        return res
+            .status(401)
+            .json({ message: `검색할 캐릭터가 서버에 존재하지 않습니다.` });
+    }
+
+    // 찾고자 하는 캐릭이 내캐릭이 아닐 경우
+    if (searchCharacter.userId !== req.user.id) {
+        return res
+            .status(200)
+            .json({
+                "name": searchCharacter.characterName,
+                "health": searchCharacter.hp,
+                "str": searchCharacter.str,
+                "dex": searchCharacter.dex,
+                "int": searchCharacter.int
+            });
+    }
+    else {
+        return res
+            .status(200)
+            .json({
+                "name": searchCharacter.characterName,
+                "health": searchCharacter.hp,
+                "str": searchCharacter.str,
+                "dex": searchCharacter.dex,
+                "int": searchCharacter.int,
+                "money": searchCharacter.money
+            });
+    }
 });
 
 export default router;
