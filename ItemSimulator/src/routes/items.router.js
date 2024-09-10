88be5bc045264e9dcc92d843431458d453caa0d9
성원 정 <tyhg56@naver.com>
2024-09-10 23:21:15 +0900

    Fix : " 아이템 수정 추가 "
    items.router.js
     - router.post('/item-edit/:itemCode') 추가
      - 수정하고자 하는 아이템을 DB에서 찾고 못찾으면 에러 반환
    
      - itemPrice를 수정하지 못하게 받아온 editData에서 itemPrice를 제거함
    
      - itemStr, itemDex, itemInt 값이 설정되어 있지 않으면 0으로 초기화
    
      - itemTable 업데이트 수행

--- ItemSimulator/src/routes/items.router.js
+++ ItemSimulator/src/routes/items.router.js
@@ -3,23 +3,27 @@ import { prisma } from '../utils/prisma/prismaClient.js'
 
 const router = express.Router();
 
+// 아이템 생성
 router.post('/item-create', async (req, res, next) => {
     const { itemCode, itemName, itemStat, itemPrice } = req.body;
 
+    // 생성할 아이템이 이미 있는지 확인
     const isExistItem = await prisma.items.findFirst({
         where: {
             itemCode: itemCode
         }
     });
 
+    // 생성할 아이템이 이미 있으면 에러 반환
     if (isExistItem) {
         return res
             .status(401)
             .json({ message: `${itemName}은 이미 존재하는 아이템입니다.` });
     }
 
+    // Items 테이블에 아이템 생성
     const dbNewItem = await prisma.items.create({
-        data: {                
+        data: {
             itemCode: itemCode,
             itemName: itemName,
             itemPrice: itemPrice,
@@ -32,6 +36,56 @@ router.post('/item-create', async (req, res, next) => {
     return res
         .status(200)
         .json({ message: `${itemName} 아이템을 만들었습니다.` });
+});
+
+// 아이템 수정
+router.post('/item-edit/:itemCode', async (req, res, next) => {
+    const { itemCode } = req.params;
+    const editData = req.body;
+
+    // 아이템을 찾음
+    const searchItem = await prisma.items.findFirst({
+        where: {
+            itemCode: +itemCode
+        }
+    });
+
+    // 못찾으면 에러 반환
+    if (!searchItem) {
+        return res
+            .status(401)
+            .json({ message: `수정할 아이템이 서버에 존재하지 않습니다.` });
+    }
+
+    // 가격 속성 삭제해서 가격 수정할 수 없게 함
+    delete editData.itemPrice;
+
+    // 아이템 나머지 능력치가 정해지지 않을 경우 각 데이터를 0으로 초기화
+    if (editData.itemStr === undefined) {
+        editData.itemStr = 0;
+    }
+
+    if (editData.itemDex === undefined) {
+        editData.itemDex = 0;
+    }
+
+    if (editData.itemInt === undefined) {
+        editData.itemInt = 0;
+    }
+
+    // item 테이블 업데이트
+    await prisma.items.update({
+        where: {
+            itemCode: +itemCode
+        },
+        data: {
+            ...editData           
+        }
+    });
+
+    return res
+        .status(200)
+        .json({ message: `${itemCode} 아이템 수정 성공` });
 });
 
 export default router;
