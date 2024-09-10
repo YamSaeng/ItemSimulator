6306e2769b751a5bbb20f89f932c349609d902a4
성원 정 <tyhg56@naver.com>
2024-09-10 20:53:52 +0900

    Fix : " 아이템 생성 추가 "
    items.router.js
     - router.post('/item-create') 추가 ( 아이템 생성 )
      - 아이템 정보를 받아 이미 있는지 확인하고, 없으면 DB에 저장

--- ItemSimulator/src/routes/items.router.js
+++ ItemSimulator/src/routes/items.router.js
@@ -1,5 +1,37 @@
 import express from 'express';
+import { prisma } from '../utils/prisma/prismaClient.js'
 
 const router = express.Router();
+
+router.post('/item-create', async (req, res, next) => {
+    const { itemCode, itemName, itemStat, itemPrice } = req.body;
+
+    const isExistItem = await prisma.items.findFirst({
+        where: {
+            itemCode: itemCode
+        }
+    });
+
+    if (isExistItem) {
+        return res
+            .status(401)
+            .json({ message: `${itemName}은 이미 존재하는 아이템입니다.` });
+    }
+
+    const dbNewItem = await prisma.items.create({
+        data: {                
+            itemCode: itemCode,
+            itemName: itemName,
+            itemPrice: itemPrice,
+            itemStr: itemStat.itemStr,
+            itemDex: itemStat.itemDex,
+            itemInt: itemStat.itemInt
+        }
+    });
+
+    return res
+        .status(200)
+        .json({ message: `${itemName} 아이템을 만들었습니다.` });
+});
 
 export default router;
