2a20ded62c1c6af3c6a5f428e042fb7d5d63777c
성원 정 <tyhg56@naver.com>
2024-09-10 13:06:15 +0900

    Fix : " 캐릭터 생성 추가 "
    characters.router.js
     - router.post('/character-create' ) 추가
      - 캐릭터를 생성함
       - accessToken을 가지고 있지 않으면 에러메세지 보냄
    
       - accessToken의 유효기간이 만료되면 에러메세지 보냄
    
       - 생성할 캐릭터의 이름이 이미 있는지 확인하고 없으면 캐릭터 생성 후 DB에 저장

--- ItemSimulator/src/routes/characters.router.js
+++ ItemSimulator/src/routes/characters.router.js
@@ -1,5 +1,54 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+import { prisma } from '../utils/prisma/prismaClient.js'
+import { ValidateToken } from '../routes/users.router.js'
+import dotenv from "dotenv";
 
 const router = express.Router();
+
+dotenv.config();
+
+const accessTokenSecretKey = process.env.ACCESS_TOKEN_SECRET_KEY;
+const refreshTokenSecretKey = process.env.REFRESH_TOKEN_SECRET_KEY;
+
+router.post('/character-create', async (req, res, next) => {
+    const { characterName } = req.body;
+
+    const c2sAccessToken = req.cookies.accessToken;    
+
+    if (c2sAccessToken == undefined) {
+        return res.status(404).send('not found');
+    }
+
+    const payLoadAccess = ValidateToken(c2sAccessToken, accessTokenSecretKey);
+    if (!payLoadAccess) {        
+        return res.status(401).json({ message: '세션이 만료되었습니다. 로그인을 다시 하세요.' });
+    }
+
+    const isExistCharacter = await prisma.characters.findFirst({
+        where: { characterName: characterName }
+    });
+
+    if (isExistCharacter) {
+        return res.status(401).json({ message: `${characterName}은 이미 존재하는 캐릭터 이름입니다.` })
+    }
+
+    const dbNewCharacter = await prisma.characters.create({
+        data: {
+            userId: payLoadAccess.id, // 요청한 id를 바탕으로 캐릭터를 생성해줌
+            characterName: characterName,
+            hp: 50,
+            str: 4,
+            dex: 4,
+            int: 4,
+            money: 10000
+        }
+    });
+
+    return res
+        .status(200)
+        .json({ message: `${characterName} 캐릭터를 만들었습니다.` });
+});
 
 export default router;
