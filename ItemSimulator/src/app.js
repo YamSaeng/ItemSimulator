e09d08a1422662ac07c3d24513714d7fecdcc2f7
성원 정 <tyhg56@naver.com>
2024-09-10 13:00:58 +0900

    Fix : " Characters Router 등록 "
    app.js
     - Characters Router 등록

--- ItemSimulator/src/app.js
+++ ItemSimulator/src/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import UsersRouter from './routes/users.router.js';
+import CharactersRouter from './routes/characters.router.js';
 
 const app = express();
 const PORT = 3020;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/ItemSimulator', [UsersRouter]);
+app.use('/ItemSimulator', [UsersRouter],[CharactersRouter]);
 
 app.listen(PORT, () => {
     console.log(PORT, "아이템 시뮬레이터 서버 열림");
