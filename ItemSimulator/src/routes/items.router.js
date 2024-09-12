f087d244249355b207eb68d9a444ac390f3a9f84
성원 정 <tyhg56@naver.com>
2024-09-12 09:14:49 +0900

    Fix : " 아이템 구입 추가 "
    items.router.js 수정
     - router.post('item/buyItem/:itemBuyCharacterCode') 추가
      - 로그인한 유저가 유효한 대상인지 authMiddleware를 통해 판정
    
      - 아이템 구입을 요청한 캐릭터가 DB에 있는지 확인
        구입하려는 아이템이 Item DB에 있는지 확인
        캐릭터가 아이템을 구매하기에 돈이 충분한지 확인
        인벤토리를 찾아 구입하려는 아이템을 인벤토리에 넣음

--- ItemSimulator/src/routes/items.router.js
+++ ItemSimulator/src/routes/items.router.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/prismaClient.js'
+import authMiddleware from '../../middlewares/auth.middleware.js'
 
 const router = express.Router();
 
@@ -116,15 +117,15 @@ router.get('/item/:itemCode', async (req, res, next) => {
     // itemCode로 item 테이블에서 아이템 찾음
     const item = await prisma.items.findFirst({
         where: {
-            itemCode : +itemCode
+            itemCode: +itemCode
         },
         select: {
             itemCode: true,
             itemName: true,
             itemPrice: true,
-            itemStr:true,
+            itemStr: true,
             itemDex: true,
-            itemInt:true,
+            itemInt: true,
         }
     })
 
@@ -139,6 +140,121 @@ router.get('/item/:itemCode', async (req, res, next) => {
     return res
         .status(200)
         .json({ item });
+});
+
+// 아이템 구입
+router.post('/item/buyItem/:itemBuyCharacterCode', authMiddleware, async (req, res, next) => {
+    const { itemBuyCharacterCode } = req.params;
+    const { itemCode, count } = req.body;
+
+    // 구입하고자 하는 유저의 캐릭터를 찾음
+    const character = await prisma.characters.findFirst({
+        where: {
+            characterId: +itemBuyCharacterCode
+        }
+    })
+
+    // 캐릭터가 DB에 없으면 에러 반환
+    if (!character) {
+        return res.status(401).json({ message: "아이템을 구매하려는 캐릭터를 서버에서 찾을 수 없습니다." })
+    }
+
+    // 캐릭터가 로그인한 유저의 캐릭터인지 확인
+    if (character.userId !== req.user.id) {
+        return res
+            .status(401)
+            .json({ message: `유효한 접근이 아닙니다.` });
+    }
+
+    // 아이템이 DB에 있는지 확인
+    const searchItem = await prisma.items.findFirst({
+        where: {
+            itemCode: +itemCode
+        }
+    });
+
+    // 구매하려는 아이템이 DB에 없으면 에러 반환
+    if (!searchItem) {
+        return res
+            .status(401)
+            .json({ message: `구매하려는 아이템 코드 ${itemCode}를 찾을 수 없습니다.` });
+    }
+
+    // 구입하려는 아이템의 총 비용 계산
+    const buyItemSumPrice = searchItem.itemPrice * count;
+    if (buyItemSumPrice > character.money) { // 돈이 부족할 경우 에러 반환
+        return res
+            .status(401)
+            .json({ message: `돈이 부족해 아이템을 구입할 수 없습니다.` })
+    }
+    else {
+        // 캐릭터의 인벤토리를 찾는다.
+        const chracterInventory = await prisma.inventory.findFirst({
+            where: {
+                characterId: character.characterId
+            }
+        });
+
+        // 캐릭터의 인벤토리가 DB에 없으면 에러 반환
+        if (!chracterInventory) {
+            return res
+                .status(401)
+                .json({ message: `[itembuy] 인벤토리를 찾을 수 없습니다.` })
+        }
+
+        // 인벤토리에 이미 해당 아이템을 가지고 있는지 확인한다.
+        const inventoryItem = await prisma.inventoryItem.findFirst({
+            where: {
+                itemId: searchItem.itemId
+            }
+        });
+
+        // 인벤토리에 아이템이 이미 있으면 개수를 증가시켜준다.
+        if (inventoryItem) {
+            await prisma.inventoryItem.update({
+                where: {
+                    inventoryItemIndex: inventoryItem.inventoryItemIndex
+                },
+                data:{
+                    inventoryItemCount: inventoryItem.inventoryItemCount + count
+                }
+            });
+        }
+        else { // 인벤토리에 아이템이 없으면 인벤토리에서 빈공간을 찾은 후 아이템을 넣어준다.
+            const emptyInventoryItem = await prisma.inventoryItem.findFirst({
+                where: {
+                    inventoryId: chracterInventory.inventoryId,
+                    itemId : null
+                }
+            });
+
+            await prisma.inventoryItem.update({
+                where: {
+                    inventoryItemIndex: emptyInventoryItem.inventoryItemIndex
+                },
+                data: {
+                    itemId: searchItem.itemId,
+                    inventoryItemCount: count
+                }
+            })
+        }
+
+        // 잔돈을 계산해 character의 momey를 업데이트 한다.
+        const change = character.money - buyItemSumPrice;
+
+        const moneyUpdateCharacter = await prisma.characters.update({
+            where: {
+                characterId: character.characterId
+            },
+            data: {
+                money: change
+            }
+        });
+
+        return res
+            .status(200)
+            .json({ message: `아이템 구입 성공! 남은 돈 : ${moneyUpdateCharacter.money}` });
+    }
 });
 
 export default router;
