6cb0803e060a4d6a3d5590cf39509fcc620df6f3
성원 정 <tyhg56@naver.com>
2024-09-12 18:38:02 +0900

    Fix : " 인벤토리 라우터 등록 "
    app.js 수정
     - 인벤토리 라우터 등록

--- ItemSimulator/src/app.js
+++ ItemSimulator/src/app.js
@@ -3,13 +3,14 @@ import cookieParser from 'cookie-parser';
 import UsersRouter from './routes/users.router.js';
 import CharactersRouter from './routes/characters.router.js';
 import ItemRouter from './routes/items.router.js'
+import InventoryRouter from './routes/inventory.router.js'
 
 const app = express();
 const PORT = 3020;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/ItemSimulator', [UsersRouter], [CharactersRouter], [ItemRouter]);
+app.use('/ItemSimulator', [UsersRouter], [CharactersRouter], [ItemRouter], [InventoryRouter]);
 
 app.listen(PORT, () => {
     console.log(PORT, "아이템 시뮬레이터 서버 열림");
